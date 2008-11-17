38eb5b1864619ec60d4036accf500690f152541c
Matt Aimonetti <mattaimonetti@gmail.com>
2008-11-16 19:15:03 -0500

    add_model_specs

--- my-first-app/spec/models/article_spec.rb
+++ my-first-app/spec/models/article_spec.rb
@@ -2,6 +2,10 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Article do
 
-  it "should have specs"
+  it "should not be valid without a title" do
+    @article = Article.new
+    @article.should_not be_valid
+  end
+
 
 end
